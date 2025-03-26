dea8d6ca7949b910e1a04885543b0a1fa7f2a09a
Youssef Ajroudi <youssef.ajroudi03@gmail.com>
2025-03-26 12:54:03 +0100

    intial commit

---
+++ Temp mesure code/Temp mesure code.ino
@@ -0,0 +1,29 @@
+
+#include <LiquidCrystal_I2C.h>
+#include <DHT.h>
+
+LiquidCrystal_I2C LCD(0x27, 16, 2);
+DHT CAP(7,  DHT11);
+
+void setup() {
+  Wire.begin();
+  CAP.begin();
+  LCD.init();
+  LCD.backlight();
+  LCD.setCursor(0,0);
+  LCD.print("Hello");
+  LCD.setCursor(0,1);
+  LCD.print("Project by yssf");
+  delay(1000);
+}
+
+void loop() {
+  float temp = CAP.readTemperature();
+  float hum = CAP.readHumidity();
+  LCD.clear();
+  LCD.setCursor(0,0);
+  LCD.print("Temp = " + String(temp) + "C");
+  LCD.setCursor(0,1);
+  LCD.print("Hum = " + String(hum) + "%");
+  delay(500);
+}
